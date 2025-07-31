8b1de9d968419e508b59ff3ebc9d749470ef63e7
Faisal Husain <faisalhusain1320@gmail.com>
2025-07-31 23:00:27 +0530

    fixed :  css issue fixed

--- src/components/gradient-screen.tsx
+++ src/components/gradient-screen.tsx
@@ -22,7 +22,7 @@ const GradientScreen = () => {
   };
 
   return (
-    <div className="h-full flex flex-col gap-5 mt-6 md:mt-0 items-center justify-center">
+    <div className="h-full flex flex-col gap-2 md:gap-5 mt-6 md:mt-0 items-center justify-center">
       <div
         className="aspect-[16/9] w-full max-w-[500px] max-h-[500px] border border-zinc-800/20 dark:border-zinc-800 rounded-[10px] overflow-hidden shadow-lg p-4 flex items-center justify-center"
         style={{
