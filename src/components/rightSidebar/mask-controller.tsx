ad5e22a4281ac8f85942f58fed64d11ddbaa9bc8
Faisal Husain <faisalhusain1320@gmail.com>
2025-08-26 21:38:34 +0530

    wip : still doing

--- src/components/rightSidebar/mask-controller.tsx
+++ src/components/rightSidebar/mask-controller.tsx
@@ -7,7 +7,14 @@ import ColorPicker from "../color-picker";
 import { motion, AnimatePresence } from "framer-motion"
 import AnimatedSlider from "../percentage-slider"
 import { Switch } from "../ui/switch";
-
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from "@/components/ui/select"
+import PointSelector from "../custom-ui/axis-selector";
 
 const MaskControls = () => {
   const {
@@ -26,6 +33,11 @@ const MaskControls = () => {
     setViaPercentage,
     toPercentage,
     setToPercentage,
+    radialShape,
+    maskType,setMaskType,
+    setRadialShape,
+    radialPosition,
+    setRadialPosition
 
   } = useMaskStore();
 
@@ -73,10 +85,78 @@ const MaskControls = () => {
           </AnimatePresence>
           <ColorPicker label="To" value={to} onChange={setTo} />
         </div>
+             <div className="flex flex-col gap-2 w-full">
+                <div className="font-bold text-sm w-full text-left">
+                    Gradient Type
+                </div>
+                <Select onValueChange={setMaskType} defaultValue={maskType}>
+                    <SelectTrigger className="w-full text-xs ">
+                        <SelectValue placeholder="Select Gradient Type" />
+                    </SelectTrigger>
+                    <SelectContent>
+                        <SelectItem value="linear">Linear</SelectItem>
+                        <SelectItem value="radial">Radial</SelectItem>
+                    </SelectContent>
+                </Select>
+                <AnimatePresence mode="wait">
+                    {maskType === "radial" && (
+                        <motion.div
+                            key="radial"
+                            initial={{ opacity: 0, y: 16 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            exit={{ opacity: 0, y: -16 }}
+                            transition={{ duration: 0.4, ease: "easeInOut" }}
+                        >
+                            <div className="flex flex-col gap-2 w-full">
+                                <div className="font-bold text-sm w-full text-left">
+                                    Radial Shape
+                                </div>
+                                <Select onValueChange={setRadialShape} defaultValue={radialShape}>
+                                    <SelectTrigger className="w-full text-xs ">
+                                        <SelectValue placeholder="Select a radial shape" />
+                                    </SelectTrigger>
+                                    <SelectContent>
+                                        <SelectItem value="circle">Circle</SelectItem>
+                                        <SelectItem value="ellipse">Ellipse</SelectItem>
+                                    </SelectContent>
+                                </Select>
+                            </div>
+                            <div
+                                className=" mt-5"
+                            >
+                                
+                                <PointSelector position={radialPosition} setPosition={setRadialPosition} />
+                            </div>
+                        </motion.div>
+                    )}
+                </AnimatePresence>
+
+                {/* {maskType === "linear" &&
+                    <div className="flex flex-col gap-2 w-full">
+                        <div className="font-bold text-sm w-full text-left">
+                            Gradient Direction
+                        </div>
+                        <Select onValueChange={setDirection} defaultValue={direction}>
+                            <SelectTrigger className="w-full text-xs ">
+                                <SelectValue placeholder="Select a direction" />
+                            </SelectTrigger>
+                            <SelectContent>
+                                {Object.entries(CSS_DIRECTION_VALUES).map(([value, label]) => (
+                                    <SelectItem key={value} value={value}>
+                                        {label}
+                                    </SelectItem>
+                                ))}
+                            </SelectContent>
+                        </Select>
+                    </div>
+                } */}
+
+
+            </div>
         <div className="flex h-full   w-full p-3">
 
         <AnimatedSlider label="From" defaultValue={fromPercentage} onChange={(value) => setFromPercentage(value)} />
-        <AnimatedSlider label="From" defaultValue={viaPercentage} onChange={(value) => setViaPercentage(value)} />
+        <AnimatedSlider label="From" defaultValue={viaPercentage} onChange={(value) => setViaPercentage(value)} disabled={!via}  />
         <AnimatedSlider label="From" defaultValue={toPercentage} onChange={(value) => setToPercentage(value)} />
       </div>
       </div>
